a2b00a607e2cec60580c50b28aa4a13d7085d5a1
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-07-10 15:40:39 +0900

    limitationを200にする

--- cpurun.sh
+++ cpurun.sh
@@ -7,5 +7,5 @@ VOCAB_SOURCE=wakati/summarize-source-vocab.txt
 VOCAB_TARGET=wakati/summarize-target-vocab.txt
 
 python -u train.py -g -1 -o result-cpu \
-       --max-source-sentence 2000 \
+       --max-source-sentence 200 \
        $SOURCE $TARGET $VOCAB_SOURCE $VOCAB_TARGET
