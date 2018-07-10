061e4b3b4b3892fa5943ebf0967ba7f57fc9d4ed
Takatsugu Nokubi <takatsugu.nokubi@robotfund.co.jp>
2018-07-10 13:32:45 +0900

    gpu実行引数

---
+++ gpurun.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+
+SOURCE=wakati/preprocessed-summarize-source.txt
+TARGET=wakati/preprocessed-summarize-target.txt
+VOCAB_SOURCE=wakati/summarize-source-vocab.txt
+VOCAB_TARGET=wakati/summarize-target-vocab.txt
+
+python -u train.py -g 0 -o result-cpu \
+       --max-source-sentence 2000 \
+       $SOURCE $TARGET $VOCAB_SOURCE $VOCAB_TARGET
